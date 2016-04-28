6f4809f5bd4452a616fdabff0108d4a5bf9977d1
ctapia <ctapia@UTP062457.grupoutp.net>
2016-04-28 11:39:32 -0500

    App preguntados

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,11 @@
-var app = (function(){
+var app = (function () {
 
     var estado,
         myScroll,
         myScrollMenu,
         preload = {
-            cargaOK : false,
-            imageURL : "../img/ring.svg"
+            cargaOK: false,
+            imageURL: "../img/ring.svg"
         };
 
     preload.imagen = new Image();
@@ -17,59 +17,63 @@ var app = (function(){
         var pass = $('#txtpass').val();
 
         $.post("http://10.30.15.218/API_Preguntados/login/", {
-            user : user,
-            pass : pass
+            user: user,
+            pass: pass
         })
-        .done(function(data){
+        .done(function (data) {
             var data = eval(data);
-            if(!data){
+            if (!data) {
                 $('#message').text('Error de inicio de sesion');
-            }else{
+            } else {
                 window.localStorage.setItem("name", data[0].firstname);
 
                 var options = {
-                    "direction"      : "right", // 'left|right|up|down', default 'right' (Android currently only supports left and right)
-                    "duration"       :  600, // in milliseconds (ms), default 400
-                    "iosdelay"       :   50, // ms to wait for the iOS webview to update before animation kicks in, default 60
-                    "androiddelay"   :  70,  // same as above but for Android, default 70
-                    "winphonedelay"  :  150, // same as above but for Windows Phone, default 200
+                    "direction": "right", // 'left|right|up|down', default 'right' (Android currently only supports left and right)
+                    "duration": 600, // in milliseconds (ms), default 400
+                    "iosdelay": 50, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                    "androiddelay": 70, // same as above but for Android, default 70
+                    "winphonedelay": 150, // same as above but for Windows Phone, default 200
                     //"href"           : "main-view/list-courses.html"
-                    "href"           : "ranking/ranking.html"
+                    "href": "ranking/ranking.html"
                 };
                 window.plugins.nativepagetransitions.flip(
-                    options,
-                    function (msg) {console.log("success: " + msg)},
-                    function (msg) {alert("error: " + msg)}
+                        options,
+                        function (msg) {
+                            console.log("success: " + msg)
+                        },
+                        function (msg) {
+                            alert("error: " + msg)
+                        }
                 );
             }
         });
     }
 
     function StyleApp(topParam) {
-        var heightCuerpo=window.innerHeight-46;
+        var heightCuerpo = window.innerHeight - 46;
         var style = document.createElement('style');
         style.type = 'text/css';
-        style.innerHTML = '.auxCSS { position:absolute; z-index:2; left:0; top:' + 
+        style.innerHTML = '.auxCSS { position:absolute; z-index:2; left:0; top:' +
                 topParam + 'px; width:100%; height: ' + heightCuerpo + 'px; overflow:auto;}';
 
         document.getElementsByTagName('head')[0].appendChild(style);
     }
 
-    function addClass( classname, element ) {
+    function addClass(classname, element) {
         var cn = element.className;
-        if( cn.indexOf( classname ) != -1 ) {
+        if (cn.indexOf(classname) != -1) {
             return;
         }
-        if( cn != '' ) {
-            classname = ' '+classname;
+        if (cn != '') {
+            classname = ' ' + classname;
         }
-        element.className = cn+classname;
+        element.className = cn + classname;
     }
 
-    function removeClass( classname, element ) {
+    function removeClass(classname, element) {
         var cn = element.className;
-        var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
-        cn = cn.replace( rxp, '' );
+        var rxp = new RegExp("\\s?\\b" + classname + "\\b", "g");
+        cn = cn.replace(rxp, '');
         element.className = cn;
     }
 
@@ -79,22 +83,22 @@ var app = (function(){
         StyleApp(92);
 
         $('#cuerpo').addClass('page center');
-        $('#menuprincipal').css({'display' : 'block'}).addClass('page center');
+        $('#menuprincipal').css({'display': 'block'}).addClass('page center');
         $('#wrapper').addClass('auxCSS');
 
         $.get('../opciones/opcion1.html')
-        .done(function(data){
+        .done(function (data) {
             $('#contenidoCuerpo').html(data);
         });
 
         // Creamos los 2 scroll mediante el plugin iscroll, uno para el menœ principal y otro para el cuerpo
-        myScroll = new iScroll('wrapper', { hideScrollbar: true });
-        myScrollMenu = new iScroll('wrapperMenu', { hideScrollbar: true });
-        
+        myScroll = new iScroll('wrapper', {hideScrollbar: true});
+        myScrollMenu = new iScroll('wrapperMenu', {hideScrollbar: true});
+
         new FastClick(document.body);
     }
 
-    function menuSlide( opt ) {
+    function menuSlide(opt) {
         $('#cuerpo').addClass('page transition');
 
         if (opt == "menu") {
@@ -106,45 +110,45 @@ var app = (function(){
                 estado = "cuerpo";
             }
         } else {
-            addClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opt]);
+            addClass('li-menu-activo', document.getElementById("ulMenu").getElementsByTagName("li")[opt]);
 
             $.get("../opciones/opcion" + opt + ".html")
-            .done(function(data) {
+            .done(function (data) {
                 $('#contenidoCuerpo').html(data);
             });
 
             myScroll.refresh();
-            myScroll.scrollTo(0,0);
+            myScroll.scrollTo(0, 0);
 
             $('#cuerpo').removeClass('right').addClass('center');
             estado = "cuerpo";
 
-            setTimeout(function() {
-                removeClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opt]);
+            setTimeout(function () {
+                removeClass('li-menu-activo', document.getElementById("ulMenu").getElementsByTagName("li")[opt]);
             }, 300);
         }
     }
 
     function ListCourses() {
-        
+
         StyleApp(45);
 
         $('#wrapper').addClass('auxCSS');
         $('#listaCursos').html($("<center style='padding:11%;'></center>").append(preload.imagen));
         /*$.mobile.loading('show', {
-            text : 'Cargando...',
-            textVisible : true,
-            theme : 'a',
-            textonly : false
-        });*/
+         text : 'Cargando...',
+         textVisible : true,
+         theme : 'a',
+         textonly : false
+         });*/
 
         $.ajax({
-            type : 'GET',
-            url : "http://10.30.15.218/API_Preguntados/list-courses/",
-            contentType : "application/json; charset=utf-8",
-            dataType : "json"
+            type: 'GET',
+            url: "http://10.30.15.218/API_Preguntados/list-courses/",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
         })
-        .done(function(data){
+        .done(function (data) {
             var data = eval(data);
             var lista = '<div class="list">';
             for (var i = 0; i < data.length; i++) {
@@ -152,23 +156,23 @@ var app = (function(){
                 lista += '<i class="icon ion-ios-redo-outline"></i>' + data[i].title;
                 lista += '</a>';
             }
-            
+
             lista += '</div>';
 
             $('#listaCursos').html(lista);
 
-            myScroll = new iScroll('wrapper', { hideScrollbar: true });
+            myScroll = new iScroll('wrapper', {hideScrollbar: true});
             //$.mobile.loading( "hide" );
         });
-        
+
         new FastClick(document.body);
     }
 
     return {
-        login           : login,
-        ListCourses     : ListCourses,
-        InitmenuSlide   : InitmenuSlide,
-        menuSlide       : menuSlide
+        login: login,
+        ListCourses: ListCourses,
+        InitmenuSlide: InitmenuSlide,
+        menuSlide: menuSlide
     };
 
 })();
